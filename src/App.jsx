77aabfdb0d4f989bd6b702c1a40a131d07a9c668
Ammar bashir haruna <ammarbashirharuna2020@gmail.com>
2024-07-18 16:04:29 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Counter from './components/counter'
+import './global.css'
+function App() {
+
+
+  return (
+    <main className='container'>
+     <Counter />
+     
+    </main>
+  )
+}
+
+export default App
